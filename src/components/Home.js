770248db92d3af7f8784c16c9a7a0220c091631c
reza <rezahajipour@gmail.com>
2022-06-27 13:16:46 +0200

    React Scroll

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import "../styles/Home.css";
 import logo from "../images/5.png";
+import { Link } from "react-scroll";
 
 const Home = () => {
     return (
@@ -9,12 +10,29 @@ const Home = () => {
                 <h1 className="hajipour">Reza Hajipour</h1>
                 <h2 className="Fullstack">Fullstack Web Developer</h2>
                 <div className="btn_container">
-                    <a href="#about">
-                        <button className="about_btn">ABOUT ME</button>
-                    </a>
-                    <a href="#work">
-                        <button className="work_btn">RECENT WORK</button>
-                    </a>
+                    <Link
+                        activeClass="active"
+                        to="about"
+                        spy={true}
+                        smooth={true}
+                        offset={0}
+                        duration={1000}
+                        className="about_btn"
+                    >
+                        ABOUT ME
+                    </Link>
+
+                    <Link
+                        activeClass="active"
+                        to="work"
+                        spy={true}
+                        smooth={true}
+                        offset={0}
+                        duration={1000}
+                        className="work_btn"
+                    >
+                        RECENT WORK
+                    </Link>
                 </div>
             </div>
         </section>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 import "../styles/Nav.css";
 import React, { useState } from "react";
 import menuIcon from "../images/burgerMenu.png";
+import { Link } from "react-scroll";
 
 function Nav() {
     const [openMenu, setOpenMenu] = useState(false);
@@ -21,24 +22,56 @@ function Nav() {
                 <nav className="nav open">
                     <ul>
                         <li>
-                            <a href="#home">
-                                <p>HOME</p>
-                            </a>
+                            <Link
+                                activeClass="active"
+                                to="home"
+                                spy={true}
+                                smooth={true}
+                                offset={-100}
+                                duration={500}
+                                className="navlink"
+                            >
+                                HOME
+                            </Link>
                         </li>
                         <li>
-                            <a href="#about">
-                                <p>ABOUT</p>
-                            </a>
+                            <Link
+                                activeClass="active"
+                                to="about"
+                                spy={true}
+                                smooth={true}
+                                offset={0}
+                                duration={1000}
+                                className="navlink"
+                            >
+                                ABOUT
+                            </Link>
                         </li>
                         <li>
-                            <a href="#work">
-                                <p>WORK</p>
-                            </a>
+                            <Link
+                                activeClass="active"
+                                to="work"
+                                spy={true}
+                                smooth={true}
+                                offset={0}
+                                duration={1000}
+                                className="navlink"
+                            >
+                                WORK
+                            </Link>
                         </li>
                         <li>
-                            <a href="#contact">
-                                <p>CONTACT</p>
-                            </a>
+                            <Link
+                                activeClass="active"
+                                to="contact"
+                                spy={true}
+                                smooth={true}
+                                offset={0}
+                                duration={1000}
+                                className="navlink"
+                            >
+                                CONTACT
+                            </Link>
                         </li>
                     </ul>
                 </nav>
